f1519a8e1b0a5bd437288d9e93e6cc5ba979afc0
Sam Haycraft <phreakdawg@gmail.com>
2014-06-27 14:18:39 -0600

    initial projectsetup

---
+++ ChatNodeJS/public/js/chat_ui.js
@@ -0,0 +1,3 @@
+/**
+ * @author Sam Haycraft
+ */
---
+++ ChatNodeJS/server.js
@@ -0,0 +1,9 @@
+/**
+ * @author Sam Haycraft
+ */
+
+var http = require ('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var cache = {};
