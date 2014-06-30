7c8997eacf3d9044b3f1473de6d43e25b98e9f7c
Sam Haycraft <phreakdawg@gmail.com>
2014-06-29 21:18:10 -0600

    server changes

--- ChatNodeJS/server.js
+++ ChatNodeJS/server.js
@@ -7,3 +7,18 @@ var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
 var cache = {};
+
+function send404(response){
+	response.writeHead(404, {'Content-Type': 'text/plain'});
+	response.write('Error 404: resource not found you fucking asshole');
+	response.end();
+}
+
+function sendFile(response, filePath, fileContents) {
+	response.writeHead(
+		200,
+		{'Content-Type': mime.lookup(path.basename(filePath))}
+	);
+}
+
+function 
