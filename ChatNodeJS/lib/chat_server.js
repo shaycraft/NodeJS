4489153bf583d7b1add8c7025731d86ae388a4a7
Sam Haycraft <phreakdawg@gmail.com>
2014-07-11 14:10:07 -0600

    typo

--- ChatNodeJS/lib/chat_server.js
+++ ChatNodeJS/lib/chat_server.js
@@ -12,7 +12,7 @@ var currentRoom = {};
 exports.listen = function(server) {
 	io = socketio.listen(server);
 	io.set('log level', 1);
-	io.sockets.on('connection', function(socket {
+	io.sockets.on('connection', function(socket) {
 		guestNumber = assignGuestName(socket, guestNumber, nickNames, namesUsed);
 		joinRoom(socket, 'Lobby');
 		
