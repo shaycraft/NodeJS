2298e0ac56078feaf3dd5b4027c7384b7e435839
Sam Haycraft <phreakdawg@gmail.com>
2014-07-11 12:13:23 -0600

    added connection handling

--- ChatNodeJS/lib/chat_server.js
+++ ChatNodeJS/lib/chat_server.js
@@ -1,3 +1,29 @@
 /**
  * @author Sam Haycraft
  */
+
+var socketio = require('socket.io');
+var io;
+var guestNumber = 1;
+var nickNames = {};
+var namesUsed = [];
+var current Room = {};
+
+exports.listen = function(server) {
+	io = socketio.listen(server);
+	io.set('log level', 1);
+	io.sockets.on('connection', function(socket {
+		guestNumber = assignGuestName(socket, guestNumber, nickNames, namesUsed);
+		joinRoom(socket, 'Lobby');
+		
+		handleMessageBroadcasting(socket, nickNames);
+		handleNameChangeAttempts(socket, nickNames, namesUsed);
+		
+		socket.on('rooms', function() {
+			socket.emit('rooms', io.sockets.manager.rooms);
+		});
+		
+		handleClientDisconnection(socket, nickNames, namesUsed);		
+		
+	});
+}
